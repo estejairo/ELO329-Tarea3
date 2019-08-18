69ae4acc72226f1aa7d7f3f900735e9453d08f87
jairo.gonzalez.13 <jairo.gonzalez.13@Aragorn.elo.utfsm.cl>
2019-08-17 21:51:12 -0300

    Removed unuseful files

--- Stage2/MyTimer/MyListener.h
+++
@@ -1,8 +0,0 @@
-#include "Listener.h"
-class MyListener:public Listener {
-public:
-   MyListener();
-   void actionPerformed();
-private:
-   int i;
-};
--- Stage2/StreetTrafficLight.cpp
+++
@@ -1,9 +0,0 @@
-#include "TrafficLight.cpp"
-class StreetTrafficLight : public TrafficLight{
-public:
-
-    StreetTrafficLight(int tf,int tt);
-
-};
-StreetTrafficLight::StreetTrafficLight(int tf,int tt):TrafficLight(tf,tt){
-}
